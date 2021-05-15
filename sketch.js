bb1e4080978aeffd1fdf400abb24e1946085b3ce
disha2613 <82595224+disha2613@users.noreply.github.com>
2021-05-15 22:39:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 
 var canvas,bg;
 var together;
-var cat, catImg1,catImg2;
-var mouse, mouseImg1,mouseImg2;
-var bgImg;
-var cat, catImg, catImg2, catImg3;
-var mouse, mouseImg, mouseImg2, mouseImg3;
+var cat, catImg1,catImg2,catImg3;
+var mouse, mouseImg1,mouseImg2,catImg3;
 
 function preload() {
     bg = loadImage("garden.png");
@@ -30,33 +27,19 @@ function setup(){
     mouse.addAnimation("mouseStanding", mouseImg1);
     mouse.scale = 0.15;
 
-    createCanvas(1000,800);
-    //create tom and jerry sprites here
-    cat = createSprite(800,700);
-    cat.addAnimation("cat_resting",catImg);
-    cat.scale = 0.1;
     
-    mouse = createSprite(400,700);
-    mouse.scale = 0.1;
-    mouse.addAnimation("mouse_resting",mouseImg);
-
-}
 
 function draw() {
 
 
     background(bg);
-
-    background(bgImg);
     
     //Write condition here to evalute if tom and jerry collide
-
-
     if(cat.x - mouse.x < (cat.width - mouse.width)/2)
     { 
-        cat.velocityX=0;
+        cat.velocityX = 0;
         cat.addAnimation("catLastImage", catImg3);
-        cat.x =300;
+        cat.x = 300;
         cat.scale=0.2;
         cat.changeAnimation("catLastImage");
         mouse.addAnimation("mouseLastImage", mouseImg3);
