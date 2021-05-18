604088522b3570502d4d94a870223a0224a940f0
disha2613 <82595224+disha2613@users.noreply.github.com>
2021-05-18 19:31:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 
 var canvas,bg;
 var together;
-var cat, catImg1,catImg2,catImg3;
-var mouse, mouseImg1,mouseImg2,catImg3;
+var cat, catImg1, catImg2, catImg3;
+var mouse, mouseImg1, mouseImg2, mouseImg3;
 
 function preload() {
     bg = loadImage("garden.png");
@@ -27,23 +27,23 @@ function setup(){
     mouse.addAnimation("mouseStanding", mouseImg1);
     mouse.scale = 0.15;
 
-    
+}
 
 function draw() {
 
 
     background(bg);
-    
+
     //Write condition here to evalute if tom and jerry collide
+
+
     if(cat.x - mouse.x < (cat.width - mouse.width)/2)
     { 
         cat.velocityX = 0;
         cat.addAnimation("catLastImage", catImg3);
         cat.x = 300;
-        cat.scale=0.2;
         cat.changeAnimation("catLastImage");
         mouse.addAnimation("mouseLastImage", mouseImg3);
-        mouse.scale=0.15;
         mouse.changeAnimation("mouseLastImage");
     }  
 
@@ -57,7 +57,7 @@ function keyPressed(){
         cat.velocityX = -5; 
         cat.addAnimation("catRunning", catImg2);
         cat.changeAnimation("catRunning");
-        
+
         mouse.addAnimation("mouseTeasing", mouseImg2);
         mouse.frameDelay = 25;
         mouse.changeAnimation("mouseTeasing");
